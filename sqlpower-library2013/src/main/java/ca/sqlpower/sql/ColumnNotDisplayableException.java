c1ef532bb5c53f97bc0e8b9d062593e8ae83cc4c
jianjun.tan.cn@gmail.com <jianjun.tan.cn@gmail.com@6f89d036-be39-ba84-9d10-82c390fe7d15>
2013-02-05 08:49:01 +0000

    baseline

---
+++ sqlpower-library2013/src/main/java/ca/sqlpower/sql/ColumnNotDisplayableException.java
@@ -0,0 +1,10 @@
+package ca.sqlpower.sql;
+
+/**
+ * Throwing a ColumnNotDisplayableException indicates that a column
+ * for which data was requested is not meant for screen display.
+ *
+ * @author Jonathan Fuerth
+ * @version $Id: ColumnNotDisplayableException.java 30 2001-05-10 23:18:24Z fuerth $
+ */
+public class ColumnNotDisplayableException extends Exception {}
