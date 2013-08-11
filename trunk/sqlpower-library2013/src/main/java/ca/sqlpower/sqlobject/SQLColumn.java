7ba7b0b3927e4b42ab307f31099c18c055fa7b32
jianjun.tan.cn@gmail.com <jianjun.tan.cn@gmail.com@6f89d036-be39-ba84-9d10-82c390fe7d15>
2013-08-11 04:25:13 +0000

    Save/Load remark into/from Microsoft SQL Server/MySQL Database for SQL Power Library

--- trunk/sqlpower-library2013/src/main/java/ca/sqlpower/sqlobject/SQLColumn.java
+++ trunk/sqlpower-library2013/src/main/java/ca/sqlpower/sqlobject/SQLColumn.java
@@ -48,6 +48,8 @@ import ca.sqlpower.sql.SQL;
 import ca.sqlpower.sqlobject.SQLRelationship.SQLImportedKey;
 import ca.sqlpower.sqlobject.SQLTypePhysicalProperties.SQLTypeConstraint;
 import ca.sqlpower.sqlobject.SQLTypePhysicalPropertiesProvider.PropertyType;
+import ca.sqlpower.sqlobject.dbmeta.DatabaseMeta;
+import ca.sqlpower.sqlobject.dbmeta.DatabaseMetaFactory;
 import ca.sqlpower.util.UserPrompter;
 import ca.sqlpower.util.UserPrompterFactory;
 
@@ -474,73 +476,8 @@ public class SQLColumn extends SQLObject implements java.io.Serializable, SPVari
 	                                String table,
 	                                DatabaseMetaData dbmd) 
 		throws SQLException, DuplicateColumnException, SQLObjectException {
-		ResultSet rs = null;
-		final ListMultimap<String, SQLColumn> multimap = ArrayListMultimap.create();
- 		try {
-			logger.debug("SQLColumn.addColumnsToTables: catalog="+catalog+"; schema="+schema);
-			rs = dbmd.getColumns(catalog, schema, table, "%");
-			
-			int autoIncCol = SQL.findColumnIndex(rs, "is_autoincrement");
-			logger.debug("Auto-increment info column: " + autoIncCol);
-
-			while (rs.next()) {
-				logger.debug("addColumnsToTable SQLColumn constructor invocation.");
-				
-				String tableName = rs.getString(3);
-				
-				boolean autoIncrement;
-                if (autoIncCol > 0) {
-                    autoIncrement = "yes".equalsIgnoreCase(rs.getString(autoIncCol));
-                } else {
-                    autoIncrement = false;
-                }
-                String nativeTypeName = rs.getString(6);
-
-				if(nativeTypeName.indexOf('(') >= 0) {
-					nativeTypeName = nativeTypeName.substring(0, nativeTypeName.indexOf('('));
-				}
-                
-				SQLColumn col = new SQLColumn(null,
-											  rs.getString(4),  // col name
-											  rs.getInt(5), // data type (from java.sql.Types)
-											  nativeTypeName, // native type name
-											  rs.getInt(7), // column size (precision)
-											  rs.getInt(9), // decimal size (scale)
-											  rs.getInt(11), // nullable
-											  rs.getString(12) == null ? "" : rs.getString(12), // remarks
-											  rs.getString(13), // default value
-											  autoIncrement // isAutoIncrement
-											  );
-				logger.debug("Precision for the column " + rs.getString(4) + " is " + rs.getInt(7));
-
-				// work around oracle 8i bug: when table names are long and similar,
-				// getColumns() sometimes returns columns from multiple tables!
-				// XXX: should be moved to the JDBC Wrapper for Oracle
-				String dbTableName = rs.getString(3);
-				if (dbTableName != null) {
-					if (!dbTableName.equalsIgnoreCase(tableName)) {
-						logger.warn("Got column "+col.getName()+" from "+dbTableName
-									+" in metadata for "+tableName+"; not adding this column.");
-						continue;
-					}
-				} else {
-					logger.warn("Table name not specified in metadata.  Continuing anyway...");
-				}
-
-				logger.debug("Adding column "+col.getName());
-				
-	        	multimap.put(dbTableName, col);
-
-			}
-			
-			return multimap;
-		} finally {
-			try {
-				if (rs != null) rs.close();
-			} catch (SQLException ex) {
-				logger.error("Couldn't close result set", ex);
-			}
-		}
+        DatabaseMeta dm = DatabaseMetaFactory.creator(dbmd.getDatabaseProductName());
+        return dm.fetchColumnsWithRemark(catalog, schema, table, dbmd);
 	}
 
 	/**
